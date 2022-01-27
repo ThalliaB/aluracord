f21c5bf9dcb75a2316b3c9cf75bd1abead159a80
ThalliaB <thalliabrum60@gmail.com>
2022-01-27 17:47:19 -0300

    upload background image file

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function PaginaInicial() {
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
                     backgroundColor: appConfig.theme.colors.primary[500],
-                    backgroundImage:'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+                    backgroundImage: 'url(../img/background.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
             >
